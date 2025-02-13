c9d0e2a2fab1f7801337146da073455f1d55a031
amirmejri96 <amirmejri1996@gmail.com>
2025-02-13 01:49:28 +0100

    Responsive design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [showModal, setShowModal] = useState(false)
   return (
-    <nav>
+    <header>
+      <button className='menu icon-menu' onClick={() => setShowModal(true)} />
+      <nav>
         <ul className='flex'>
-            <li><a href='#home'>Accueil</a></li>
-            <li><a href='#about'>A propos</a></li>
-            <li><a href='#régimes'>Régimes</a></li>
-            <li><a href='#Contact'>Contact</a></li>
+          <li><a href='#home'>Accueil</a></li>
+          <li><a href='#about'>À propos</a></li>
+          <li><a href='#régimes'>Régimes</a></li>
+          <li><a href='#Contact'>Contact</a></li>
         </ul>
-    </nav>
+      </nav>
+
+      {showModal && (
+        <div className="fixed">
+          <ul className="modal">
+            <li><button onClick={() => setShowModal(false)} className='icon-close' /></li>
+            <li><a href="#home">Accueil</a></li>
+            <li><a href="#about">À propos</a></li>
+            <li><a href="#régimes">Régimes</a></li>
+            <li><a href="#Contact">Contact</a></li>
+          </ul>
+
+        </div>
+      )}
+    </header>
   )
 }
 
--- src/pages/2- a propos/Propos.js
+++ src/pages/2- a propos/Propos.js
@@ -7,40 +7,39 @@ const Propos = () => {
       <div className='a-propos'>
         <h2>C'est quoi la maladie cœliaque ?</h2>
         <p>La maladie cœliaque est une maladie inflammatoire intestinale, secondaire à l'ingestion de gluten,
-           survenant chez des patients génétiquement prédisposés
-            La consommation de gluten est responsable de la destruction des villosités intestinales,
-             qui assurent l'absorption des nutriments et des vitamines. La réaction immunitaire excessive vis-à-vis du gluten,
-              provoque une inflammation chronique. 
-              La synthèse d'anticorps en excès par le système immunitaire altère la barrière intestinale.
-               La maladie peut être découverte à tous les stades de la vie mais est souvent diagnostiquée
-                dans l'enfance ou entre 20 et 40 ans.</p>
+          survenant chez des patients génétiquement prédisposés
+          La consommation de gluten est responsable de la destruction des villosités intestinales,
+          qui assurent l'absorption des nutriments et des vitamines. La réaction immunitaire excessive vis-à-vis du gluten,
+          provoque une inflammation chronique.
+          La synthèse d'anticorps en excès par le système immunitaire altère la barrière intestinale.
+          La maladie peut être découverte à tous les stades de la vie mais est souvent diagnostiquée
+          dans l'enfance ou entre 20 et 40 ans.</p>
         <img src='../../../assets/disease.jpg' alt='La maladie coelique' />
         <h4>C'est quoi le gluten ?</h4>
         <p>Le gluten est une protéine qui se retrouve dans certaines céréales. Il contribue à la texture des
-           produits de boulangerie et autres produits dans lesquels il est utilisé.</p>
-        <p>⚠️ Céréales contenant du gluten : <br/>
-            S : Seigle <br/>
-            A : Avoine régulière (contaminée) <br/>
-            B : Blé <br/>
-            O : Orge <br/>
-            T : Triticale (croisement du blé et du seigle)</p>
-        
-        <h4>Quels sont les symptômes ?</h4>
-        <p>• Diarrhée <br/>
-           • Constipation <br/>
-           • Douleurs abdominales <br/>
-           • Ballonnement <br/>
-           • Dyspepsie (reflux gastro-œsophagien) réfractaire <br/>
-           • Selles pâles et nauséabondes <br/>
-           • Vomissements récurrents <br/>
-           • Ostéopénie/ostéoporose précoce <br/>
-           • Migraines/maux de tête <br/>
-           • Impact sur le cycle menstruel <br/> 
-           • Anémie ferriprive <br/>
-           • Autres carences nutritionnelles : vitamine D, folates, vitamine B12, magnésium <br/>
-           • Décoloration des dents ou caries dentaires…
+          produits de boulangerie et autres produits dans lesquels il est utilisé.</p>
+        <p>⚠️ Céréales contenant du gluten : <br />
+          S : Seigle <br />
+          A : Avoine régulière (contaminée) <br />
+          B : Blé <br />
+          O : Orge <br />
+          T : Triticale (croisement du blé et du seigle)</p>
 
-</p>
+        <h4>Quels sont les symptômes ?</h4>
+        <p>• Diarrhée <br />
+          • Constipation <br />
+          • Douleurs abdominales <br />
+          • Ballonnement <br />
+          • Dyspepsie (reflux gastro-œsophagien) réfractaire <br />
+          • Selles pâles et nauséabondes <br />
+          • Vomissements récurrents <br />
+          • Ostéopénie/ostéoporose précoce <br />
+          • Migraines/maux de tête <br />
+          • Impact sur le cycle menstruel <br />
+          • Anémie ferriprive <br />
+          • Autres carences nutritionnelles : vitamine D, folates, vitamine B12, magnésium <br />
+          • Décoloration des dents ou caries dentaires…
+        </p>
 
       </div>
 
