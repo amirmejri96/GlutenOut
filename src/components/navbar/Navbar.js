f40e22d8900943e7cf4d22d6e8c5bb1b6300cf5c
amirmejri96 <amirmejri1996@gmail.com>
2025-04-20 13:42:41 +0100

    final commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,20 +3,29 @@ import { useTranslation } from 'react-i18next';
 import './Navbar.css';
 
 const Navbar = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const [showModal, setShowModal] = useState(false);
 
   return (
     <header>
-      <button className="menu icon-menu" onClick={() => setShowModal(true)} />
-      <nav>
-        <ul className="flex">
-          <li><a href="#home">{t('Accueil')}</a></li>
-          <li><a href="#about">{t('À propos')}</a></li>
-          <li><a href="#régimes">{t('Régimes')}</a></li>
-          <li><a href="#Contact">{t('Contact')}</a></li>
-        </ul>
-      </nav>
+      <div className="navbar-container">
+        <div className="logo-container">
+          <a href="#home">
+            <img src="../../../assets/logo2.png" alt="Logo" className="logo" />
+          </a>
+        </div>
+
+        <nav>
+          <ul className="flex">
+            <li><a href="#home">{t('Accueil')}</a></li>
+            <li><a href="#about">{t('À propos')}</a></li>
+            <li><a href="#régimes">{t('Régimes')}</a></li>
+            <li><a href="#Contact">{t('Contact')}</a></li>
+          </ul>
+        </nav>
+
+        <button className="menu icon-menu" onClick={() => setShowModal(true)} />
+      </div>
 
       {showModal && (
         <div className="fixed">
